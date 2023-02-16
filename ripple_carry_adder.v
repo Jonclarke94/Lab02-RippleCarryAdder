67855232f1bd40835ea31c33d181a2ebac5aeaf4
unknown <jonclarke94@gmail.com>
2023-02-15 22:28:35 -0800

    Prelab

--- ripple_carry_adder.v
+++ ripple_carry_adder.v
@@ -24,39 +24,9 @@ module ripple_carry_adder # ( parameter NUMBITS = 16 ) (
     output reg [NUMBITS-1:0] result,  
     output reg carryout); 
 
-    module full_adder (
-        input  wire a,
-        input  wire b,
-        input  wire carryin,
-        output wire sum,
-        output wire carryout
-    );
-        assign sum = a ^ b ^ carryin;
-        assign carryout = (a & b) | (carryin & (a ^ b));
-    endmodule
-
-    reg carry;
-    wire[NUMBITS-1:0] sum;
-
-    full_adder adder_0 (
-        .a(A[0]),
-        .b(B[0]),
-        .carryin(carryin),
-        .sum(sum[0]),
-        .carryout(carry)
-    );
-
-    for (int i = 1; i < NUMBITS; i = i + 1) begin
-        full_adder adder (
-            .a(A[i]),
-            .b(B[i]),
-            .carryin(carry),
-            .sum(sum[i]),
-            .carryout(carry)
-        );
-    end
-
-    assign result = sum;
-    assign carryout = carry;
+    // ------------------------------
+    // Insert your solution below
+    // ------------------------------ 
 
 endmodule
+
--- ripple_carry_adder_tb.v
+++ ripple_carry_adder_tb.v
@@ -103,23 +103,25 @@ module ripple_carry_adder_tb;
         end
         #10; // Wait 
         
+
+        
         // ----------------------------------------
         // Add more test cases here 
         // ----------------------------------------
-                totalTests = totalTests + 1;
-    $write("\tTest Case 1.2:   5+  7 =  12, c_out = 0 ... ");
-    A = 16'h0005;
-    B = 16'h0007;
-    expected_result = 16'h000C;
+             totalTests = totalTests + 1;
+$write("\tTest Case 1.2:   127 + 1 = 128, c_out = 0 ... ");
+A = 8'h7F;
+B = 8'h01;
+expected_result = 8'h80;
 
-    #100; // Wait 
-    if (expected_result !== result || carryout !== 1'b0) begin
-        $write("failed\n");
-        failedTests = failedTests + 1;
-    end else begin
-        $write("passed\n");
-    end
-    #10; // Wait 
+#100; // Wait 
+if (expected_result !== result || carryout !== 1'b0) begin
+    $write("failed\n");
+    failedTests = failedTests + 1;
+end else begin
+    $write("passed\n");
+end
+#10; // Wait    
 
         // ----------------------------------------
         // Tests group for Increasing Number of Bits 
@@ -129,12 +131,11 @@ module ripple_carry_adder_tb;
         // ----------------------------------------
         // Add test cases here 
         // ----------------------------------------
-            totalTests = totalTests + 1;
-    $write("\tTest Case 2.1: 0x12345678 + 0x87654321 = 0x9999999A, c_out = 0 ... ");
-    A = 32'h12345678;
-    B = 32'h87654321;
-    expected_result = 32'h9999999A;
-
+        totalTests = totalTests + 1;
+    $write("\tTest Case 2.1:   5+  7 =  12, c_out = 0 ... ");
+    A = 16'h0005;
+    B = 16'h0007;
+    expected_result = 16'h000C;
     #100; // Wait 
     if (expected_result !== result || carryout !== 1'b0) begin
         $write("failed\n");
@@ -145,10 +146,24 @@ module ripple_carry_adder_tb;
     #10; // Wait 
 
     totalTests = totalTests + 1;
-    $write("\tTest Case 2.2: 0x1234 + 0x5678 = 0x68AC, c_out = 0 ... ");
+    $write("\tTest Case 2.3: 0x1234 + 0x5678 = 0x68AC, c_out = 0 ... ");
     A = 16'h1234;
     B = 16'h5678;
     expected_result = 16'h68AC;
+
+    #100; // Wait 
+    if (expected_result !== result || carryout !== 1'b0) begin
+        $write("failed\n");
+        failedTests = failedTests + 1;
+    end else begin
+        $write("passed\n");
+    end
+    #10; // Wait 
+    totalTests = totalTests + 1;
+    $write("\tTest Case 2.2: 0x12345678 + 0x87654321 = 0x9999999A, c_out = 0 ... ");
+    A = 32'h12345678;
+    B = 32'h87654321;
+    expected_result = 32'h9999999A;
 
     #100; // Wait 
     if (expected_result !== result || carryout !== 1'b0) begin
